1018404e11b1a2f5eaf98a5e13a81c84ab958983
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-10 17:26:12 +0300

    Implement function for generating random numbers

--- src/generator.py
+++ src/generator.py
@@ -1,8 +1,32 @@
 __author__ = 'Antony Cherepanov'
 
+import random
+
+
 class Generator(object):
     def __init__(self):
-        pass
+        self.length = 16
 
-    def generate(self):
-        return "12345"
+    def generate(self, tweet):
+        if tweet is None:
+            print("Can't generate number from empty tweet")
+            return None
+
+        number = tweet.author.id_str
+        chars = list(tweet.author.screen_name)
+        for c in chars:
+            number += "{0:X}".format(ord(c))
+
+        number_elements = list(number)
+        random.shuffle(number_elements)
+
+        if len(number_elements) > self.length:
+            number_elements = number_elements[0:self.length]
+
+        while len(number_elements) < self.length:
+            number_elements.append(str(random.randint(0, 9)))
+
+        number_elements.insert(3, '-')
+        number_elements.insert(8, '-')
+        number_elements.insert(13, '-')
+        return ''.join(number_elements)
