c9b786b8a57f311dbd4db878841d80b141493a69
Antony Cherepanov <antony.cherepanov@gmail.com>
2015-11-10 15:17:32 +0300

    Update schema of DB, add functions to work with users and last message id

--- src/dbhandler.py
+++ src/dbhandler.py
@@ -25,30 +25,34 @@ class DBHandler(object):
         cursor = connection.cursor()
         cursor.execute('''CREATE TABLE consumer (
             id INTEGER PRIMARY KEY NOT NULL,
-            key TEXT NOT NULL,
-            secret TEXT NOT NULL)''')
+            key TEXT NOT NULL UNIQUE,
+            secret TEXT NOT NULL UNIQUE)''')
 
         cursor.execute('''CREATE TABLE token (
             id INTEGER PRIMARY KEY NOT NULL,
-            key TEXT NOT NULL,
-            secret TEXT NOT NULL)''')
+            key TEXT NOT NULL UNIQUE,
+            secret TEXT NOT NULL UNIQUE)''')
 
         cursor.execute('''CREATE TABLE users (
             id INTEGER PRIMARY KEY NOT NULL,
-            name TEXT NOT NULL,
-            number TEXT NOT NULL)''')
+            user_id TEXT NOT NULL UNIQUE,
+            name TEXT NOT NULL UNIQUE,
+            number TEXT NOT NULL UNIQUE)''')
+
+        cursor.execute('''CREATE TABLE last_msg (
+            id INTEGER PRIMARY KEY NOT NULL,
+            msg_id TEXT NOT NULL UNIQUE)''')
 
         connection.commit()
         connection.close()
 
     def __check_consumer_data(self):
         cursor = self.connection.cursor()
-        id = ('0',)
-        cursor.execute('SELECT * FROM consumer WHERE id=?', id)
+        cursor.execute('SELECT * FROM consumer LIMIT 1')
         if cursor.fetchone() is None:
             key, secret = self.__request_consumer_data()
-            cursor.execute('INSERT INTO consumer VALUES (?, ?, ?)',
-                           (0, key, secret))
+            cursor.execute('INSERT INTO consumer VALUES (null, ?, ?)',
+                           (key, secret))
             self.connection.commit()
 
     def __request_consumer_data(self):
@@ -59,19 +63,16 @@ class DBHandler(object):
 
     def get_consumer_data(self):
         cursor = self.connection.cursor()
-        id = ('0',)
-        cursor.execute('SELECT * FROM consumer WHERE id=?', id)
-        data = cursor.fetchone()
-        return data[1], data[2]
+        cursor.execute('SELECT key, secret FROM consumer LIMIT 1')
+        return cursor.fetchone()
 
     def __check_token_data(self):
         cursor = self.connection.cursor()
-        id = ('0',)
-        cursor.execute('SELECT * FROM token WHERE id=?', id)
+        cursor.execute('SELECT * FROM token LIMIT 1')
         if cursor.fetchone() is None:
             key, secret = self.__request_token_data()
-            cursor.execute('INSERT INTO token VALUES (?, ?, ?)',
-                           (0, key, secret))
+            cursor.execute('INSERT INTO token VALUES (null, ?, ?)',
+                           (key, secret))
             self.connection.commit()
 
     def __request_token_data(self):
@@ -82,7 +83,48 @@ class DBHandler(object):
 
     def get_access_token_data(self):
         cursor = self.connection.cursor()
-        id = ('0',)
-        cursor.execute('SELECT * FROM token WHERE id=?', id)
+        cursor.execute('SELECT key, secret FROM token LIMIT 1')
+        return cursor.fetchone()
+
+    def get_last_msg_id(self):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT msg_id FROM last_msg LIMIT 1')
+        return cursor.fetchone()[0]
+
+    def set_last_msg_id(self, new_id):
+        if new_id is None or len(new_id) == 0:
+            print("Invalid ID of last message: ", new_id)
+            return
+
+        cursor = self.connection.cursor()
+        cursor.execute('UPDATE last_msg SET msg_id=? WHERE id=0', (new_id,))
+        self.connection.commit()
+
+    def get_user_data(self, user_id):
+        if user_id is None or len(user_id) == 0:
+            print("Invalid user ID: ", user_id)
+            return None
+
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT name, number FROM users WHERE user_id=?',
+                       (user_id,))
+
         data = cursor.fetchone()
-        return data[1], data[2]
+        if data is None:
+            print("There is no info in database about user with ID: ", user_id)
+            return None
+
+        user_data = {'user_id': user_id, 'name': data[0], 'number': data[1]}
+        return user_data
+
+    def add_user(self, user_data):
+        if self.get_user_data(user_data['user_id']) is not None:
+            print("User with ID {0} already exist in database".format(
+                user_data['user_id']))
+            return
+
+        cursor = self.connection.cursor()
+        cursor.execute('INSERT INTO users VALUES (null, ?, ?, ?))',
+                       (user_data['user_id'], user_data['name'],
+                        user_data['number']))
+        self.connection.commit()
